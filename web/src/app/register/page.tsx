c6de8fd84e67737afa442df404b4507515802722
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-09-08 22:42:38 -0300

    feat:adicionado campos de cadastro

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -1,3 +1,37 @@
+import LoginButton from "../components/loginButton"
+import RegisterLoginButton from "../components/registerLoginButton"
+import logo from "../public/logo.png"
+import Image from "next/image"
+
 export default function Register() {
-  return <h1>Tela de Cadastro</h1>
+  return (
+    <div className="p-0 m-0 flex">
+      <div className="h-dvh w-3/5 bg-amber-600">
+
+      </div>
+      <div className="h-dvh w-2/5 bg-[#EFEAEA] flex items-center justify-center">
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-center mb-15">
+            <Image 
+            src={logo} 
+            alt={"logo"} 
+            width={200} 
+            height={200}/>
+          </div>
+          <div className="flex justify-center mb-15">
+            <RegisterLoginButton/>
+          </div>
+          <text className="text-black justify-start">Email</text>
+          <input className="border-b-2 border-b-black w-170 mb- text-gray-700"></input>
+          <text className="text-black">Senha</text>
+          <input className="border-b-2 border-b-black w-170 mb-10 text-gray-700"></input>
+          <text className="text-black">Senha</text>
+          <input className="border-b-2 border-b-black w-170 mb-15 text-gray-700"></input>
+          <div className="flex justify-center">
+            <LoginButton/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
