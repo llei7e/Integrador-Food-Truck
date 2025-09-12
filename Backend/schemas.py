661f65961a4544a6380aa46bc3126cf98c6ef3fc
vinicinnnn <1vinizada@gmail.com>
2025-09-11 21:24:32 -0300

    feat: conectando banco de dados com o backend

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -1,28 +1,54 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
+# ================= Usuario =================
+class UsuarioBase(BaseModel):
+    nome: str
+    tipo: str
+
+class UsuarioCreate(UsuarioBase):
+    pass
+
+class UsuarioResponse(UsuarioBase):
+    id: int
+    data_criacao: Optional[datetime]
+
+    class Config:
+        from_attributes = True
+
+
+# ================= Produto =================
 class ProdutoBase(BaseModel):
     nome: str
-    preco: float
-    estoque: int
+    descricao: str
+    preco: int
+    categoria_id: int
+    ativo: bool
 
 class ProdutoCreate(ProdutoBase):
     pass
 
-class ProdutoOut(ProdutoBase):
+class ProdutoResponse(ProdutoBase):
     id: int
-    class Config:
-        orm_mode = True
 
+    class Config:
+        from_attributes = True
+
+
+# ================= Pedido =================
 class PedidoBase(BaseModel):
-    produto_id: int
-    quantidade: int
+    usuario_id: int
+    foodtruck_id: int
+    status: str
+    total: int
 
 class PedidoCreate(PedidoBase):
     pass
 
-class PedidoOut(PedidoBase):
+class PedidoResponse(PedidoBase):
     id: int
-    data: datetime
+    data_criacao: Optional[datetime]
+
     class Config:
-        orm_mode = True
+        from_attributes = True
