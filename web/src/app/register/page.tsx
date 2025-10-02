8c3ab592e63cdb5185c84c578e45ffff96ce00cd
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-01 21:27:40 -0300

    feat:add responsive screen

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -3,31 +3,39 @@ import SelectButton from "../components/selectButton"
 import logo from "../public/logo.png"
 import Image from "next/image"
 
-export default function Register() {
+export default function Login() {
   return (
-    <div className="p-0 m-0 flex">
-      <div className="h-dvh w-3/5 bg-amber-600">
+    <div className="flex h-dvh w-full">
+      <div className="hidden md:block md:w-3/5 bg-amber-600">
 
       </div>
-      <div className="h-dvh w-2/5 bg-[#EFEAEA] flex items-center justify-center">
-        <div className="flex flex-col justify-center">
+      <div className="w-full md:w-2/5 bg-[#EFEAEA] flex items-center justify-center">
+        <div className="flex flex-col justify-center w-[70%] max-w-md">
           <div className="flex justify-center mb-15">
-            <Image 
+            <Image
             priority={false}
-            src={logo}
-            alt={"logo"}
-            width={200}
+            src={logo} 
+            alt={"logo"} 
+            width={200} 
             height={200}/>
           </div>
-            <SelectButton colorButton1="bg-[#EA2626]" colorButton2="bg-[#E7E5E5]" positionButton1="z-20" positionButton2="z-10"/>
+
+          <SelectButton
+            colorButton1="bg-[#EA2626]"
+            colorButton2="bg-[#E7E5E5]"
+          />
+
           <h1 className="text-black justify-start mt-10">Email</h1>
-          <input className="border-b-2 border-b-black w-170 mb-10 text-gray-700"></input>
+          <input className="border-b-2 border-b-black w-full mb-10 text-gray-700"></input>
+          
           <h1 className="text-black">Senha</h1>
-          <input className="border-b-2 border-b-black w-170 mb-10 text-gray-700"></input>
+          <input className="border-b-2 border-b-black w-full mb-10 text-gray-700"></input>
+          
           <h1 className="text-black">Senha</h1>
-          <input className="border-b-2 border-b-black w-170 mb-10 text-gray-700"></input>
+          <input className="border-b-2 border-b-black w-full mb-10 text-gray-700"></input>
+          
           <div className="flex justify-center">
-            <Button text="Cadastrar-se" href="/login"/>
+            <Button text={"Cadastrar-se"} href={"/login"}/>
           </div>
         </div>
       </div>
