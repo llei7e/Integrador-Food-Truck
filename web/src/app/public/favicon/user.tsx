1523baaa8aa88d6c63ecc257feb28f4982bb0cad
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-02 20:38:17 -0300

    feat:change icon user size

--- web/src/app/public/favicon/user.tsx
+++ web/src/app/public/favicon/user.tsx
@@ -2,6 +2,6 @@ import { Icon } from "@iconify/react";
 
 export default function User() {
   return (
-    <Icon icon="solar:user-circle-linear" width={80} color="black"/>
+    <Icon icon="solar:user-circle-linear" width={50} color="black"/>
   );
 }
