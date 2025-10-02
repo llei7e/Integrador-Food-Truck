71f85be463db79fd19d959df8d276ee9991eb013
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-02 20:37:30 -0300

    feat:change header size

--- web/src/app/components/header.tsx
+++ web/src/app/components/header.tsx
@@ -6,12 +6,12 @@ import HeaderButton from "./headerButton";
 export default function Header() {
   return (
     <div>
-      <div className="flex items-center justify-between h-28">
+      <div className="flex items-center justify-between h-18">
         <div className="flex items-center">
           <Truck />
           <div className="text-gray-950 justify-start">
-            <h1 className="font-bold text-3xl">Food Truck</h1>
-            <h1 className="font-light text-2xl">Gerenciador</h1>
+            <h1 className="font-bold text-2xl">Food Truck</h1>
+            <h1 className="font-light">Gerenciador</h1>
           </div>
         </div>
 
