71a120c38711fc3fc2a084aa2ba7495356279f91
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-02 20:37:20 -0300

    feat:change filter size

--- web/src/app/components/filter.tsx
+++ web/src/app/components/filter.tsx
@@ -7,7 +7,7 @@ interface filterProps {
 
 export default function Filter({ placeholder, icon }: filterProps) {
   return (
-    <div className="w-100 h-12 bg-[#E7E7E7] border border-gray-500 text-[#323232] rounded-md mr-10 text-2xl flex items-center px-2">
+    <div className="w-75 h-8 bg-[#E7E7E7] border border-gray-500 text-[#323232] rounded-md mr-10 flex items-center px-2">
       {/* Texto do input alinhado à esquerda */}
       <input
         placeholder={placeholder}
