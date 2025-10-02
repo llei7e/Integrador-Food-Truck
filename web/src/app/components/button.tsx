80fa5a283feaa86c716cd49cf046247d2c52b6b4
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-01 21:27:12 -0300

    feat:add responsive screen

--- web/src/app/components/button.tsx
+++ web/src/app/components/button.tsx
@@ -5,15 +5,13 @@ interface ButtonProps {
     href: string;
 }
 
-export default function RegisterButton({text, href = " "} : ButtonProps){
+export default function Button({text, href = " "} : ButtonProps){
 
      return (
-        <>
             <Link href={href}>
-                <button className="w-100 h-20 rounded-3xl bg-[#EA2626] text-2xl cursor-pointer shadow-black shadow-lg">
+                <button className="w-50 h-20 md:h-20 md:w-80 rounded-3xl bg-[#EA2626] text-2xl cursor-pointer shadow-gray-400 shadow-lg">
                     {text}
                 </button>
             </Link>
-        </>
-     )
+     );
 }
