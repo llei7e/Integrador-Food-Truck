5480996561937327f08513d3907fa3237ccdea01
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-01 21:27:32 -0300

    feat:add responsive screen

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -5,12 +5,12 @@ import Image from "next/image"
 
 export default function Login() {
   return (
-    <div className="p-0 m-0 flex">
-      <div className="h-dvh w-3/5 bg-amber-600">
+    <div className="flex h-dvh w-full">
+      <div className="hidden md:block md:w-3/5 bg-amber-600">
 
       </div>
-      <div className="h-dvh w-2/5 bg-[#EFEAEA] flex items-center justify-center">
-        <div className="flex flex-col justify-center">
+      <div className="w-full md:w-2/5 bg-[#EFEAEA] flex items-center justify-center">
+        <div className="flex flex-col justify-center w-[70%] max-w-md">
           <div className="flex justify-center mb-15">
             <Image
             priority={false}
@@ -19,11 +19,18 @@ export default function Login() {
             width={200} 
             height={200}/>
           </div>
-          <SelectButton colorButton1="bg-[#E7E5E5]" colorButton2="bg-[#EA2626]" positionButton1="z-10" positionButton2="z-20"/>
-          <h1 className="text-black justify-start mt-10">Email</h1>
-          <input className="border-b-2 border-b-black w-170 mb-10 text-gray-700"></input>
+          
+          <SelectButton
+            colorButton1="bg-[#E7E5E5]"
+            colorButton2="bg-[#EA2626]"
+          />
+          
+          <h1 className="text-black mt-10">Email</h1>
+          <input className="border-b-2 border-b-black w-full mb-10 text-gray-700"></input>
+          
           <h1 className="text-black">Senha</h1>
-          <input className="border-b-2 border-b-black w-170 mb-10 text-gray-700"></input>
+          <input className="border-b-2 border-b-black w-full mb-10 text-gray-700"></input>
+         
           <div className="flex justify-center">
             <Button text={"Entrar"} href={"/sales"}/>
           </div>
