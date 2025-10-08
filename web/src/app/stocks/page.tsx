cf02af93b6f75509f9c3142bb14d56716a10e26d
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-10-07 21:31:25 -0300

    feat:arrumando tabela na tela de estoque

--- web/src/app/stocks/page.tsx
+++ web/src/app/stocks/page.tsx
@@ -1,14 +1,16 @@
 import Filter from "../components/filter";
 import Header from "../components/header";
+import Table from "../components/table";
 
 export default function Trucks() {
   return (
-    <div className="mr-4 ml-4">
-      <Header/>
-      <div className="flex justify-start items-center">
-        <Filter placeholder="Truck: Truck A" icon="bx:calendar"/>
-        <Filter placeholder="Data: 10/08/2025 - 20/08/2025" icon="tabler:search"/>
+      <div className="mr-4 ml-4">
+        <Header />
+        <div className="flex justify-start items-center">
+          <Filter placeholder="Truck: Truck A" icon="bx:calendar" />
+          <Filter placeholder="Data: 10/08/2025 - 20/08/2025" icon="tabler:search" />
+        </div>
+        <Table/>
       </div>
-    </div>
   )
 }
