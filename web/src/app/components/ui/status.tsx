9e1ad0bce607e5be18505e09f5d86ced2260a457
Henrique Fernandes <henrique.pereira@2rpnet.com>
2025-12-09 21:16:51 -0300

    feat:adicionado integracao com backend

--- web/src/app/components/ui/status.tsx
+++ web/src/app/components/ui/status.tsx
@@ -10,15 +10,40 @@ export default function Status({ status }: StatusProps) {
   if (status === 'Suficiente' || status === 'Concluído') {
     bg = 'bg-green-200';
     color = 'text-green-500';
+    displayText = status;
   } else if (status === 'Alerta' || status === 'Em preparo') {
     bg = 'bg-orange-200';
     color = 'text-orange-400';
+    displayText = status;
   } else if (status === 'Em falta' || status === 'Cancelado') {
     bg = 'bg-red-200';
     color = 'text-red-500';
-  } 
+    displayText = status;
+    
 
-  else if (status === 'active') {
+  } else if (status === 'FINALIZADO') {
+    bg = 'bg-green-200';
+    color = 'text-green-500';
+    displayText = 'Finalizado';
+  } else if (status === 'NA_FILA') {
+    bg = 'bg-orange-200';
+    color = 'text-orange-400';
+    displayText = 'Na fila';
+  } else if (status === 'PREPARANDO') {
+    bg = 'bg-blue-200';
+    color = 'text-blue-400';
+    displayText = 'Preparando';
+  } else if (status === 'AGUARDANDO_PAGAMENTO') {
+    bg = 'bg-blue-200';
+    color = 'text-blue-400';
+    displayText = 'Pagando';
+  } else if (status === 'CANCELADO') {
+    bg = 'bg-red-200';
+    color = 'text-red-500';
+    displayText = 'Cancelado';
+
+
+  } else if (status === 'active') {
     bg = 'bg-green-200';
     color = 'text-green-500';
     displayText = 'Ativo';
@@ -26,9 +51,7 @@ export default function Status({ status }: StatusProps) {
     bg = 'bg-red-200';
     color = 'text-red-500';
     displayText = 'Inativo';
-  } 
-
-  else if (typeof status === 'boolean') {
+  } else if (typeof status === 'boolean') {
     if (status) {
       bg = 'bg-green-200';
       color = 'text-green-500';
