d1e7791cdc81269797ea58c30f19f81baf4eafb2
Henrique Fernandes <henrique.pereira@2rpnet.com>
2025-12-11 20:37:31 -0300

    remove:deletado teste do componente map

--- web/src/app/__tests__/MapView.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MapView from '../components/map';
-
-describe('MapView', () => {
-  it('renders the button initially', () => {
-    render(<MapView />);
-    expect(screen.getByText('Visualizar mapa'));
-  });
-});
